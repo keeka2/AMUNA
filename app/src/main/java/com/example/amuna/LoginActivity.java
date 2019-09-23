6e89588af5b46bfd20eb085a1b342a8d6b4a29c4
JSKs <keeka2@naver.com>
2019-09-23 20:32:45 +0900

    간단한 회원가입&로그인

--- app/src/main/java/com/example/amuna/LoginActivity.java
+++ app/src/main/java/com/example/amuna/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.example.amuna;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,10 +22,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText Email = (EditText) findViewById(R.id.Email);
-        EditText Password = (EditText) findViewById(R.id.Password);
-        Button LoginButton = (Button) findViewById(R.id.LoginButton);
-        Button RegisterButton = (Button) findViewById(R.id.RegisterButton);
+        final EditText Email = (EditText) findViewById(R.id.Email);
+        final EditText Password = (EditText) findViewById(R.id.Password);
+        final Button LoginButton = (Button) findViewById(R.id.LoginButton);
+        final TextView RegisterButton = (TextView) findViewById(R.id.RegisterButton);
 
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +34,48 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(registerIntent);
             }
         });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String userEmail = Email.getText().toString();
+                final String userPassword = Password.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+
+                                String Email = jsonResponse.getString("Email");
+                                String Password = jsonResponse.getString("Password");
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("Email", Email);
+                                intent.putExtra("Password",Password);
+                                LoginActivity.this.startActivity(intent);
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("로그인에 실패하였습니다")
+                                        .setNegativeButton("다시 시도",null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userEmail,userPassword,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
     }
+
 }
