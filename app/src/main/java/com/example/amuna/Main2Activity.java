523b42af18ec29ab5c1cdd835757f358385592cc
ansky502 <ansky502@naver.com>
2019-10-01 17:48:02 +0900

    홈화면 추가

--- app/src/main/java/com/example/amuna/Main2Activity.java
+++ app/src/main/java/com/example/amuna/Main2Activity.java
@@ -3,69 +3,60 @@ package com.example.amuna;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+
 
 import android.view.MenuItem;
 
-import android.widget.TextView;
 
 
-public class Main2Activity extends AppCompatActivity implements OnFragmentInteractionListener {
+public class Main2Activity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private HomeFragment fragmentSearch = new HomeFragment();
+    private ChatFragment fragmentCamera = new ChatFragment();
+    private FavFragment fragmentCall = new FavFragment();
+    private AccFragment fragmentAcc = new AccFragment();
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    switchToHomeFragment();
-                    return true;
-                case R.id.navigation_favorites:
-                    mTextMessage.setText("Favorites");
-                    switchToFavFragment();
-                    return true;
-                case R.id.navigation_chat:
-                    mTextMessage.setText("Chatting");
-                    switchToChatFragment();
-                    return true;
-                case R.id.navigation_account:
-                    mTextMessage.setText("Account");
-                    switchToAccFragment();
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
     }
 
-    public void switchToHomeFragment() {
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.navigation_home, new HomeFragment()).commit();
-    }
-    public void switchToFavFragment() {
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.navigation_favorites, new FavFragment()).commit();
-    }
-    public void switchToChatFragment() {
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.navigation_chat, new ChatFragment()).commit();
-    }
-    public void switchToAccFragment() {
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.navigation_account, new AccFragment()).commit();
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.navigation_account:
+                    transaction.replace(R.id.frameLayout,fragmentAcc).commitAllowingStateLoss();
+                    break;
+
+                case R.id.navigation_home:
+                    transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
+                    break;
+                case R.id.navigation_chat:
+                    transaction.replace(R.id.frameLayout, fragmentCamera).commitAllowingStateLoss();
+                    break;
+                case R.id.navigation_favorites:
+                    transaction.replace(R.id.frameLayout, fragmentCall).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
     }
 }
