eabccd6062f22214c39472f37fd5f0b149776dcc
장유진 <wkddvwls1@gmail.com>
2019-10-02 21:44:04 +0900

    register profile ui

--- app/src/main/java/com/example/amuna/RegisterActivity.java
+++ app/src/main/java/com/example/amuna/RegisterActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -84,7 +83,7 @@ public class RegisterActivity extends AppCompatActivity {
                                         .setPositiveButton("확인",null)
                                         .create()
                                         .show();
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                Intent intent = new Intent(RegisterActivity.this, RegisterProfileActivity.class);
                                 RegisterActivity.this.startActivity(intent);
                             }
                             else
